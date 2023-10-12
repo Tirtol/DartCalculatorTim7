5e60775f1674ee7b17c2e40889a3f76df181cc79
Holis09 <137138883+Holis09@users.noreply.github.com>
2023-10-12 22:17:26 +0700

    Update tugas1kel7.dart

--- tugas1kel7/bin/tugas1kel7.dart
+++ tugas1kel7/bin/tugas1kel7.dart
@@ -8,5 +8,31 @@ void main() {
 
   stdout.write("Masukkan Bilangan Kedua = ");
   num? bil2 = num.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan Operator Perhitungan (+ - x /) = ");
+  String rumus = stdin.readLineSync() ?? '';
+
+num hasil = 0;
+
+  if (rumus == '+') {
+    hasil = bil1 + bil12;
+  } else if (rumus == '-') {
+    hasil = bil11 - bil12;
+  } else if (rumus == '*') {
+    hasil = bil11 * bil12;
+  } else if (rumus == '/') {
+    if (bil12 != 0) {
+      hasil = bil11 / bil1;
+    } else {
+      print('Pembagian nol tidak diizinkan!.');
+      return;
+    }
+  } else {
+    print('Mohon Masukan Anka yang benar !.');
+    return;
+  }
+
+  print('Hasil Perhitungan : $hasil');
+
   
 }
