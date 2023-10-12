fd9a8d68eea7a9412407c0d9dc8a4fdaf4e81a0d
nizam <choirunizam1099@gmail.com>
2023-10-12 17:13:50 +0700

    fitur input bilangan kedua

--- tugas1kel7/bin/tugas1kel7.dart
+++ tugas1kel7/bin/tugas1kel7.dart
@@ -5,4 +5,7 @@ void main() {
 
   stdout.write("Masukkan Bilangan Pertama = ");
   num? bil1 = num.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan Bilangan Kedua = ");
+  num? bil2 = num.parse(stdin.readLineSync()!);
 }
